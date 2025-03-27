3d3f909b5e5e6413ffb85e04175011a999d2309a
Carlos <carlos@flsafety.net>
2025-03-27 13:54:44 -0400

    Added Header/Footer component to Root layout. Updated MetaData. Upddated Global Font

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "@Styles/globals.css";
+import { Header, Footer } from "@Components/UI";
+
+const openSans = Open_Sans({ subsets: ['latin'] });
+
+
+export const metadata: Metadata = {
+  title: "Accoric",
+  description: "Accoric is a software company that provides solutions for businesses.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={openSans.className}
+      >
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
