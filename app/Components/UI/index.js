8e293de94092efd1b5d995e9bd5f82cb760c7b9c
Carlos <carlos@flsafety.net>
2025-03-28 16:37:38 -0400

    Add UI component exports for Header, Footer, and Buttons

---
+++ app/Components/UI/index.js
@@ -0,0 +1,6 @@
+import Header from './Header';
+import Footer from './Footer';
+import PrimaryButton from './PrimaryButton';
+import SecondaryButton from './SecondaryButton';
+
+export { Header, Footer, PrimaryButton, SecondaryButton };
