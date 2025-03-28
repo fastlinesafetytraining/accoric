6710d617b41c6985585ae5261920a2f2f515965c
Carlos <carlos@flsafety.net>
2025-03-28 16:37:49 -0400

    Add Mobile Navigation Menu component using Ant Design

---
+++ app/Components/UI/MobileNavMenu.tsx
@@ -0,0 +1,34 @@
+'use client';
+import React, { useState } from "react"
+import { Button, Drawer } from "antd"
+import { MenuOutlined } from "@ant-design/icons"
+
+export default function MobileNavMenu(): React.ReactElement {
+	const [open, setOpen] = useState(false);
+
+	const showDrawer = () => {
+		setOpen(true);
+	}
+
+	const onClose = () => {
+		setOpen(false);
+	}
+
+	return (
+		<>
+			<Button type="primary" onClick={showDrawer}><MenuOutlined /></Button>
+			<Drawer title="Mobile Menu" onClose={onClose} open={open}>
+				<ul className="mobile-nav-menu">
+					<li><a href="/accoric-management">Accoric Management</a></li>
+					<li><a href="/accoric-dashcard">Accoric DashCard</a></li>
+					<li><a href="/safety-library">Safety Library</a></li>
+					<li><a href="/why-choose-us">Why Choose Us?</a></li>
+					<li><a href="/resources">Resources</a></li>
+					<li><a href="/pricing">Pricing</a></li>
+					<li><a href="/about-us">About Us</a></li>
+				</ul>
+					
+			</Drawer>
+		</>
+	)
+}
