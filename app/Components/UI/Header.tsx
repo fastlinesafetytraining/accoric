1f090f93b053a6028138b788ff1f1859440e470e
Carlos <carlos@flsafety.net>
2025-03-28 16:34:40 -0400

    Global Header Component has been created along side Desktop Navigation Bar Menu Component

---
+++ app/Components/UI/Header.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+import NavBar from "./NavBar"
+import '@Styles/globals.css'
+
+export default function Header() {
+	return (
+		<header className="p-4 flex justify-between items-center">
+			<Link href="/">
+				<Image
+					width={200}
+					height={100}
+					src="/accoric-logo.webp"
+					alt="Accoric Logo"
+					className="min-w-[100px] max-w-[200px] h-auto"
+				/>
+			</Link>
+			<NavBar />
+			<Link className="login-button" href="https://accoricmgt.com/">Login</Link>
+		</header>
+	);
+}
---
+++ app/Components/UI/NavBar.tsx
@@ -0,0 +1,60 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+import { DownOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Dropdown, Space } from 'antd';
+import styles from '@Styles/component.module.scss';
+
+
+const items: MenuProps['items'] = [
+    {
+        key: '1',
+        label: (
+            <Link className="text-lg px-4 py-1" href='/accoric-management'>Accoric Management</Link>
+        )
+    },
+    {
+        key: '2',
+        label: (
+            <Link className="text-lg px-4 py-1" href='/accoric-dashcard'>Accoric DashCard</Link>
+        )
+    },
+    {
+        key: '3',
+        label: (
+            <Link className='text-lg px-4 py-1' href='/safety-library'>Safety Library</Link>
+        )
+    }
+];
+
+export default function NavBar(): React.ReactElement {
+    return (
+			<nav className={styles.siteDesktopNavBar}>
+				<ul className={styles.navListMenu}>
+					<li>
+						<Dropdown className={styles.navListSubmenu} menu={{ items }}>
+							<a className='cursor-pointer' onClick={(e) => e.preventDefault()}>
+								<Space>
+									Products
+									<DownOutlined style={{ fontSize: '0.75rem' }} />
+								</Space>
+							</a>
+						</Dropdown>
+					</li>
+					<li>
+						<Link href="/why-choose-us">Why Choose Us?</Link>
+					</li>
+					<li>
+						<Link href="/resources">Resources</Link>
+					</li>
+					<li>
+						<Link href="/pricing">Pricing</Link>
+					</li>
+					<li>
+						<Link href="/about-us">About Us</Link>
+					</li>
+				</ul>
+			</nav>
+		);
+};
