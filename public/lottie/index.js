a4ab472e1a7b1f223076fc92f3055990701e05fe
Carlos <carlos@flsafety.net>
2025-04-04 15:00:27 -0400

    Add Lottie animations for ease of use, quick setup, and simplicity to enhance user experience

---
+++ public/lottie/index.js
@@ -0,0 +1,7 @@
+import easeOfUseLottie from "./easeOfUse.json";
+import quickSetupLottie from "./quickSetup.json";
+import simplisticLottie from "./simplistic.json";
+
+export { easeOfUseLottie, quickSetupLottie, simplisticLottie };
+
+
