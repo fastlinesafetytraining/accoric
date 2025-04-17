e429054cd371f0c2ba4bc92e260f1420186e2536
Carlos <carlos@flsafety.net>
2025-04-17 14:53:34 -0400

    Add navigation menu routes

---
+++ app/Utils/const/routes.ts
@@ -0,0 +1,27 @@
+export const navMenuItems = [
+  {
+    key: "1",
+    label: "Accoric Management",
+    href: "/accoric-management",
+  },
+  {
+    key: "2",
+    label: "Accoric DashCard",
+    href: "/accoric-dashcard",
+  },
+  {
+    key: "3",
+    label: "Pricing",
+    href: "/pricing",
+  },
+  {
+    key: "4",
+    label: "Resources",
+    href: "/resources",
+  },
+  {
+    key: "5",
+    label: "About Us",
+    href: "/about-us",
+  },  
+];
