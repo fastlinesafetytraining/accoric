d3ba7102f68a65dd68f4e4ed8a26194f172a271e
Carlos <carlos@flsafety.net>
2025-04-17 11:22:18 -0400

    New lottie animation for the accoric management page

--- public/lottie/index.js
+++ public/lottie/index.js
@@ -1,7 +1,9 @@
 import easeOfUseLottie from "./easeOfUse.json";
 import quickSetupLottie from "./quickSetup.json";
 import simplisticLottie from "./simplistic.json";
+import nfcLottie from "./nfc.json";
+import snapshotLottie from "./snapshot.json";
+import notificationsLottie from "./notifications.json";
+import automationLottie from "./automate.json";
 
-export { easeOfUseLottie, quickSetupLottie, simplisticLottie };
-
-
+export { easeOfUseLottie, quickSetupLottie, simplisticLottie, nfcLottie, snapshotLottie, notificationsLottie, automationLottie };
