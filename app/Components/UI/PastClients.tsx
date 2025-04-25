1dcab535ed9a1f108857cf8c2adfa6e110f5f549
Carlos <carlos@flsafety.net>
2025-04-25 14:07:44 -0400

    Updated PastClients component to use dynamic image sizes based on client data, and modified SCSS styles to set image dimensions to auto for improved responsiveness.

--- app/Components/UI/PastClients.tsx
+++ app/Components/UI/PastClients.tsx
@@ -1,17 +1,14 @@
-'use client';
-
 import Image from "next/image";
 import pastClients from "@/app/utils/const/pastClient";
 import styles from '@Styles/component.module.scss';
 
 
 const PastClients = () => {
-
     return (
         <div className={styles.clientContainer}>
             {pastClients.map((client, index) => (
                 <div aria-label={client.name} key={index}>
-                    <Image width={225} height={225} className={styles.clientImage} src={client.image} alt={client.name} />
+                    <Image sizes="(max-width: 768px) 150px" width={client.width} height={client.height} className={styles.clientImage} src={client.image} alt={client.name} />
                 </div>
             ))}
         </div>
