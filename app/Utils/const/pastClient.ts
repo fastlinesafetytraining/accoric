d095d4f5f69879853e7993b023469cdb57ad87a4
Carlos <carlos@flsafety.net>
2025-04-25 14:06:09 -0400

    moved client logos into one folder updated const to include image width and height.

--- app/Utils/const/pastClient.ts
+++ app/Utils/const/pastClient.ts
@@ -1,46 +1,62 @@
-import milconLogo from "@/public/Images/milcon-logo.webp";
-import championLogo from "@/public/Images/champion-scc-logo.webp";
-import posillicoLogo from "@/public/Images/posillico-logo.webp";
-import hauglandLogo from "@/public/Images/haugland-group-logo.webp";
-import kendrionLogo from "@/public/Images/kedrion-biopharma-logo.webp";
-import dubnerLogo from "@/public/Images/dubner-landscaping-logo.webp";
-import cacLogo from "@/public/Images/cac-logo.webp";
+import milconLogo from "@/public/Images/client-logos/milcon-logo.webp";
+import championLogo from "@/public/Images/client-logos/champion-scc-logo.webp";
+import posillicoLogo from "@/public/Images/client-logos/posillico-logo.webp";
+import hauglandLogo from "@/public/Images/client-logos/haugland-group-logo.webp";
+import kendrionLogo from "@/public/Images/client-logos/kedrion-biopharma-logo.webp";
+import dubnerLogo from "@/public/Images/client-logos/dubner-landscaping-logo.webp";
+import cacLogo from "@/public/Images/client-logos/cac-logo.webp";
 
 import type { StaticImageData } from "next/image";
 
 interface PastClient {
     name: string;
     image: StaticImageData;
+    width: number;
+    height: number;
 }
 
 const pastClients: PastClient[] = [
     {
         name: "Milcon",
         image: milconLogo,
+        width: 200,
+        height: 200,
     },
     {
         name: "Champion SCC",
         image: championLogo,
+        width: 200,
+        height: 200,
     },
     {
         name: "Posillico",
         image: posillicoLogo,
+        width: 200,
+        height: 200,
     },
     {
         name: "Haugland Construction",
         image: hauglandLogo,
+        width: 200,
+        height: 200,
     },
     {
         name: "Kendrion Biopharma",
         image: kendrionLogo,
+        width: 200,
+        height: 200,
     },
     {
         name: "Dubner Landscaping & Construction",
-        image: dubnerLogo
+        image: dubnerLogo,
+        width: 200,
+        height: 200,
     },
     {
         name: "CAC Industries Inc",
-        image: cacLogo
+        image: cacLogo,
+        width: 200,
+        height: 200,
     }
 
 ];
