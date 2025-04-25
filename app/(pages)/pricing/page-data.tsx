bcb96fe2a2cecc1b048fa0893bb30e1f5dab675a
Carlos <carlos@flsafety.net>
2025-04-25 16:32:22 -0400

    completed pricing page

---
+++ app/(pages)/pricing/page-data.tsx
@@ -0,0 +1,36 @@
+export const includedInAllSubscriptions = [
+    "Unlimited Certifications",
+    "Unlimited Certification Types",
+    "Unlimited File Attachments",
+    "Unlimited Categories",
+    "Spreadsheet Functionality",
+    "Multiple User Access",
+    "Email/Text Support",
+  ];
+
+export const faqItems = [
+    {
+      question: "How do I get started?",
+      answer: "Fill out the form on this page and we will reach out to you!",
+    },
+    {
+      question: "Do I have to send you my data?",
+      answer:
+        "No, it is not required for you to send us your data. You can set up your account on your own. However, our speciality is setting up your account for you, so all you have to do is sign into a fully set up account. We receive your data in strict compliance with our privacy policy, and we never share it with any third parties.",
+    },
+    {
+      question: "Do I Pay for inactive employees?",
+      answer:
+        "There is an option to mark employees active or inactive on your Accoric Management account. You pay for all employees loaded into your account, regardless of their active status. The active/inactive function is especially useful for employers that hire seasonal or union workers that leave and come back to a company frequently.",
+    },
+    {
+      question: "Do you offer Monthly Subscriptions?",
+      answer:
+        "No, we only offer Yearly Subscriptions. However, you can cancel your subscription at any time.",
+    },
+    {
+      question: "What kind of payments do you accept?",
+      answer:
+        "We accept all common payment methods such as Credit/Debit card, Check, ACH, and purchase order.",
+    },
+  ];
---
+++ app/(pages)/pricing/page.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { PricingForm, FAQ, PastClients } from "@Components/UI";
+import { TbSquareRoundedCheckFilled } from "react-icons/tb";
+import { includedInAllSubscriptions, faqItems } from "./page-data";
+
+import styles from "@Styles/pricing.module.scss";
+
+export default function Page() {
+  return (
+    <main className={styles.pricing}> 
+      <section className={styles.pricing__header}>
+        <h1>Pricing</h1>
+        <p>
+          We offer a <span>range of pricing</span> options to suit your needs.
+        </p>
+        <p>Please contact us to discuss your requirements.</p>
+      </section>
+      <div className={styles.pricing__content}>
+      <section className={styles.pricing__includedList}>
+          <div className={styles.pricing__includedList__container}>
+            <h2>Whats Included</h2>
+            <ul className={styles.pricing__includedList__container__list}>
+              {includedInAllSubscriptions.map((item, index) => (
+                <li key={index}>
+                  <i>
+                    <TbSquareRoundedCheckFilled />
+                  </i>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+        <section className={styles.pricing__formContainer}>
+          <PricingForm />
+        </section>
+        <section className={styles.pricing__clients}>
+        <h2>Start tracking your workers' <span>certifications</span></h2>
+          <p>
+            Plus, we upload all of your data and create your account for you!
+            All you do is sign in.
+          </p>
+          <PastClients />
+        </section>
+        <section className={styles.pricing__faq}>
+          <FAQ items={faqItems} />
+        </section>
+      </div>
+    </main>
+  );
+}
