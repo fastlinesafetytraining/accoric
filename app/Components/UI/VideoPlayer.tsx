f337b04d2abf93116ef909bbabbc8ff584a43382
Carlos <carlos@flsafety.net>
2025-04-30 14:26:48 -0400

    Added optional className prop to VideoPlayer component for enhanced styling flexibility.

--- app/Components/UI/VideoPlayer.tsx
+++ app/Components/UI/VideoPlayer.tsx
@@ -4,13 +4,14 @@ import styles from "@Styles/component.module.scss";
 interface VideoPlayerProps extends React.HTMLAttributes<HTMLVideoElement> {
     src: string;
     ariaLabel: string;
+    className?: string;
 }
 
-export default function VideoPlayer({src, ariaLabel}: VideoPlayerProps) {
+export default function VideoPlayer({src, ariaLabel, className }: VideoPlayerProps) {
 
     return (    
         <>
-            <video muted autoPlay loop className={styles.videoPlayer} src={src} aria-label={ariaLabel}/>
+            <video muted autoPlay loop className={`${styles.videoPlayer} ${className}`} src={src} aria-label={ariaLabel}/>
         </>
     )
 } 
