8dcc60f66537e5451297d9643031fb66fafbf760
Carlos <carlos@flsafety.net>
2025-05-01 11:43:17 -0400

    Refactored NavBar component to integrate mobile navigation functionality, replacing the MobileNavMenu with a Drawer for improved user experience.

--- app/Components/UI/NavBar.tsx
+++ app/Components/UI/NavBar.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+'use client';
+import "@ant-design/v5-patch-for-react-19";
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { Button, Drawer } from "antd";
 import { navMenuItems } from '@Utils/const/routes';
+import { FaBars } from "react-icons/fa";
 import styles from '@Styles/component.module.scss';
+import PrimaryButton from "./PrimaryButton";
 
 export default function NavBar(): React.ReactElement {
+	const [menuActive, setMenuActive] = useState(false);
+	
+	const showMenu = () => {
+		setMenuActive(true);
+	}
+
+	const hideMenu = () => {
+		setMenuActive(false);
+	}
+	
     return (
+		<>
 			<nav className={styles.desktopNavBar}>
 				<ul className={styles.desktopNavBar__menu}>
 					{navMenuItems.map((item) => (
@@ -14,5 +30,18 @@ export default function NavBar(): React.ReactElement {
 					))}
 				</ul>
 			</nav>
+			<nav className={styles.mobileNavBar}>
+				<Button type="primary" onClick={showMenu}><FaBars /></Button>
+				<Drawer title="Welcome to Accoric!" closeIcon={false} onClose={hideMenu} open={menuActive}>
+
+				<ul className={styles.mobileNavBar__menu}>
+					{navMenuItems.map((item) => (
+						<li className={styles.mobileNavBar__menu__Item} key={item.key}>{item.label}</li>
+					))}
+				</ul>
+				<PrimaryButton href="https://accoricmgt.com/" title="Login">Login</PrimaryButton>
+				</Drawer>
+			</nav>
+		</>
 		);
 };
