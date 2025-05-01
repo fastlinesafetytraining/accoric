be02793481c6cd70bc36bbabdf5860e79847e238
Carlos <carlos@flsafety.net>
2025-05-01 12:32:35 -0400

    Refactored PastClients component to use consistent import casing and added type annotations for client properties to enhance type safety.

--- app/Components/UI/PastClients.tsx
+++ app/Components/UI/PastClients.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
-import pastClients from "@/app/utils/const/pastClient";
-import styles from '@Styles/component.module.scss';
+import pastClients from "@utils/const/pastClient";
+import styles from '@styles/component.module.scss';
+import { StaticImageData } from "next/image";
 
 
 const PastClients = () => {
     return (
         <div className={styles.clientContainer}>
-            {pastClients.map((client, index) => (
+            {pastClients.map((client: { name: string; image: StaticImageData; width: number; height: number }, index: number) => (
                 <div aria-label={client.name} key={index}>
                     <Image width={client.width} height={client.height} className={styles.clientImage} src={client.image} alt={client.name} />
                 </div>
