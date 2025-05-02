9c43bf0440ba662753d9650b27e28cf3027c37f3
Carlos <carlos@flsafety.net>
2025-05-02 13:59:26 -0400

    Added MDX support to Next.js configuration by including page extensions for .mdx files.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+
+/* @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
+  pageExtensions: ["ts", "tsx", "mdx"],
   /* config options here */
 };
 
