881a096f51e9aeb53ce7357c467efc582933dbe8
Carlos <carlos@flsafety.net>
2025-05-05 15:43:45 -0400

    Refactor MDX components to consolidate styling and enhance component usage in useMDXComponents function.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,21 @@
 import type { MDXComponents } from "mdx/types";
 
-export function useMDXComponents(): MDXComponents {
+const components: MDXComponents = {
+    h1: ({ children }) => <h1 className="text-2xl font-bold">{children}</h1>,
+    h2: ({ children }) => <h2 className="text-xl font-bold">{children}</h2>,
+    h3: ({ children }) => <h3 className="text-lg font-bold">{children}</h3>,
+    p: ({ children }) => <p className="text-base">{children}</p>,
+    a: ({ children, href }) => <a href={href} className="text-blue-500">{children}</a>,
+};
+
+
+export function useMDXComponents(initialComponents: MDXComponents): MDXComponents {
     return {
-        h1: ({ children }) => <h1 className="text-2xl font-bold">{children}</h1>,
-        h2: ({ children }) => <h2 className="text-xl font-bold">{children}</h2>,
-        h3: ({ children }) => <h3 className="text-lg font-bold">{children}</h3>,
-        p: ({ children }) => <p className="text-base">{children}</p>,
-        a: ({ children, href }) => <a href={href} className="text-blue-500">{children}</a>,
+        ...initialComponents,
+        ...components,
     };
+}
+
+export default function MDXContent({ children, ...props}: any) {
+    return <>{children}</>
 }
