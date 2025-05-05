dde6bd77be096bffb781a90469ffd7286ea77d90
Carlos <carlos@flsafety.net>
2025-05-05 13:21:55 -0400

    Created redirect list from old production sites

---
+++ libs/utils/conts/redirects.ts
@@ -0,0 +1,49 @@
+
+interface Redirect {
+    source: string;
+    destination: string;
+    permanent: boolean;
+}
+
+export const redirects: Redirect[] = [
+{
+    source: "/Home/DashCardPage",
+    destination: "/accoric-dashcard",
+    permanent: true,
+  },
+  {
+    source: "/Home/ManagementPage",
+    destination: "/accoric-management",
+    permanent: true,
+  },
+  {
+    source: "/Home/PricingPage",
+    destination: "/pricing",
+    permanent: true,
+  },
+  {
+    source: "/ContactUs/Index",
+    destination: "/contact-us",
+    permanent: true,
+  },
+  {
+    source: "/ContactUs",
+    destination: "/contact-us",
+    permanent: true,
+  },
+  {
+    source: "/HelpGuide/SupportPage",
+    destination: "/support",
+    permanent: true,
+  },
+  {
+    source: "/privacy-policy/index",
+    destination: "/privacy-policy",
+    permanent: true,
+  },
+  {
+    source: "/TermsOfService/index",
+    destination: "/terms-of-service",
+    permanent: true,
+  },
+]
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
+import { redirects } from "@/libs/utils/conts/redirects";
 
 /* @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx", "mdx"],
   /* config options here */
+  redirects: async () => [
+    ...redirects,
+  ],
 };
 
 const withMDX = createMDX({
