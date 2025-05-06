71a7b11c85c378d86c1852a7941189025bd50675
Carlos <carlos@flsafety.net>
2025-05-06 09:45:06 -0400

    Add Google Tag Manager integration with scripts in layout.tsx and include NoScriptGoogleTag component for improved tracking.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import { ReactLenis } from "lenis/react";
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "@styles/globals.css";
 import { Header, Footer } from "@components/UI";
+import NoScriptGoogleTag from "@components/NoScriptGoogleTag";
 const openSans = Open_Sans({ subsets: ['latin'] });
 
 
@@ -35,11 +37,27 @@ export default function RootLayout({
       <html lang="en">
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+          				{/* Google Tag Manager */}
+          <Script
+					async
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_PRODUCTION_GOOGLE_TAG_MANAGER_ID}`}
+				></Script>
+				<Script
+					dangerouslySetInnerHTML={{
+						__html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                    `,
+					}}
+				></Script>
+				{/* End Google Tag Manager */}
         </head>
         <body className={openSans.className}>
         <Header />
         {children}
         <Footer />
+        <NoScriptGoogleTag />
       </body>
     </html>
     </ReactLenis>
