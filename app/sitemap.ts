6a37b041334db138f2c4947abcbc82e19fd1f2ca
Carlos <carlos@flsafety.net>
2025-05-06 13:48:33 -0400

    Add sitemap generation functionality with dynamic URLs and metadata for improved SEO

---
+++ app/sitemap.ts
@@ -0,0 +1,149 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+    const defaultImageUrl = `${baseUrl}/Images/accoric-social-media-image.svg`;
+
+	return [
+		{
+			url: `${baseUrl}`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 1,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/pricing`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.8,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/about-us`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/terms-of-service`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/privacy-policy`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/accoric-dashcard`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/accoric-management`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/support`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/support/using-the-employee-table`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/support/searching-for-employees`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/support/adding-employees`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+        {
+            url: `${baseUrl}/support/adding-certifications`,
+            lastModified: new Date().toISOString(),
+            changeFrequency: "weekly",
+            priority: 0.5,
+            images: [`${defaultImageUrl}`]
+        },
+		{
+			url: `${baseUrl}/support/updating-employees`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/support/using-expired-certification-table`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/support/active-and-inactive-employees`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/support/using-notes`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/support/using-categories`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/support/generating-spreadsheets`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/support/secondary-users`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+		{
+			url: `${baseUrl}/support/forgot-password`,
+			lastModified: new Date().toISOString(),
+			changeFrequency: "weekly",
+			priority: 0.5,
+            images: [`${defaultImageUrl}`]
+		},
+	];
+}
