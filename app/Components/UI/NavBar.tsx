a2a828f4725e95fee541962c63b7755b05e3e77e
Carlos <carlos@flsafety.net>
2025-05-06 10:40:23 -0400

    Made improvements on the navbar to change dynamicly when the window inner width changes it will return the proper navigation created for either desktop or mobile screen sizes

--- app/Components/UI/NavBar.tsx
+++ app/Components/UI/NavBar.tsx
@@ -1,47 +1,82 @@
-'use client';
+"use client";
 import "@ant-design/v5-patch-for-react-19";
-import React, { useState } from 'react';
-import Link from 'next/link';
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
 import { Button, Drawer } from "antd";
-import { navMenuItems } from '@/libs/utils/conts/routes';
+import { navMenuItems } from "@/libs/utils/conts/routes";
 import { FaBars } from "react-icons/fa";
-import styles from '@styles/component.module.scss';
+import styles from "@styles/component.module.scss";
 import PrimaryButton from "./PrimaryButton";
 
 export default function NavBar(): React.ReactElement {
 	const [menuActive, setMenuActive] = useState(false);
-	
+	const [isMobile, setIsMobile] = useState(typeof window !== 'undefined' ? window.innerWidth < 768 : false);
+
 	const showMenu = () => {
 		setMenuActive(true);
-	}
+	};
 
 	const hideMenu = () => {
 		setMenuActive(false);
-	}
-	
-    return (
-		<>
-			<nav className={styles.desktopNavBar}>
-				<ul className={styles.desktopNavBar__menu}>
-					{navMenuItems.map((item) => (
-						<li className={styles.desktopNavBar__menu__Item} key={item.key}>
-							<Link className={styles.desktopNavBar__menu__Item__Link} href={item.href}>{item.label}</Link>
-						</li>
-					))}
-				</ul>
-			</nav>
-			<nav className={styles.mobileNavBar}>
-				<Button type="primary" onClick={showMenu}><FaBars /></Button>
-				<Drawer title="Welcome to Accoric!" closeIcon={false} onClose={hideMenu} open={menuActive}>
+	};
 
-				<ul className={styles.mobileNavBar__menu}>
-					{navMenuItems.map((item) => (
-						<li className={styles.mobileNavBar__menu__Item} key={item.key}>{item.label}</li>
-					))}
-				</ul>
-				<PrimaryButton href="https://accoricmgt.com/" title="Login">Login</PrimaryButton>
-				</Drawer>
-			</nav>
+	useEffect(() => {
+		const handleWindowResize = () => {
+			setIsMobile(window.innerWidth < 768);
+		};
+
+		handleWindowResize();
+
+		window.addEventListener("resize", handleWindowResize);
+
+		return () => {
+			window.removeEventListener("resize", handleWindowResize);
+		};
+	}, []);
+
+	return (
+		<>
+			{isMobile ?  (
+				// Mobile Navbar
+				<nav className={styles.mobileNavBar}>
+					<Button type='primary' onClick={showMenu}>
+						<FaBars />
+					</Button>
+					<Drawer
+						title='Welcome to Accoric!'
+						closeIcon={false}
+						onClose={hideMenu}
+						open={menuActive}
+					>
+						<ul className={styles.mobileNavBar__menu}>
+							{navMenuItems.map((item) => (
+								<li className={styles.mobileNavBar__menu__Item} key={item.key}>
+									{item.label}
+								</li>
+							))}
+						</ul>
+						<PrimaryButton href='https://accoricmgt.com/' title='Login'>
+							Login
+						</PrimaryButton>
+					</Drawer>
+				</nav>
+			) : (
+				// Desktop Navbar
+				<nav className={styles.desktopNavBar}>
+					<ul className={styles.desktopNavBar__menu}>
+						{navMenuItems.map((item) => (
+							<li className={styles.desktopNavBar__menu__Item} key={item.key}>
+								<Link
+									className={styles.desktopNavBar__menu__Item__Link}
+									href={item.href}
+								>
+									{item.label}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</nav>
+			)}
 		</>
-		);
-};
+	);
+}
