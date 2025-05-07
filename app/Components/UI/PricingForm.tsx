27397ac2a7ca6360a3bc7baf396e4a60d060ec57
Carlos <carlos@flsafety.net>
2025-05-07 12:03:49 -0400

    Refactor PricingForm component by extracting form fields, marks, and radio options into a new utility file for better organization and maintainability. Update form submission logic to reset fields upon success.

--- app/Components/UI/PricingForm.tsx
+++ app/Components/UI/PricingForm.tsx
@@ -1,239 +1,186 @@
 "use client";
+import "@ant-design/v5-patch-for-react-19";
 import React, { useEffect, useState } from "react";
 import { emailFormSubmission } from "@/app/api/emailSubmissionApi";
-import "@ant-design/v5-patch-for-react-19";
-import type { SliderSingleProps } from "antd/es/slider";
+import {
+	formFields,
+	marks,
+	radioOptions,
+} from "@/libs/utils/conts/pricingFormData";
 import { App, Form, Input, Button, Slider, Radio, ConfigProvider } from "antd";
-import { FaBuilding, FaUser, FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 import styles from "@styles/component.module.scss";
 
 export default function PricingForm() {
-  const [form] = Form.useForm();
-  const [selectedValue, setSelectedValue] = useState<number>(5);
-  const [priceValue, setPriceValue] = useState<string>("$799/Year");
+	const [form] = Form.useForm();
+	const [selectedValue, setSelectedValue] = useState<number>(5);
+	const [priceValue, setPriceValue] = useState<string>("$799/Year");
 
-  const { useApp } = App;
-  const { notification: appNotification } = useApp();
+	const { useApp } = App;
+	const { notification: appNotification } = useApp();
 
-  const formSubmission = async () => {
-    const values = form.getFieldsValue();
-    await emailFormSubmission(values)
-    .then((response: {status: number, message: string}) => {
-      appNotification.success({
-        placement: "top",
-        message: "Email sent successfully",
-        description: response.message,
-      });
-    })
-    .catch((error: {message: string}) => {
-      appNotification.error({
-        placement: "top",
-        message: "Email sent failed",
-        description: error.message,
-      });
-    });
-  };
+	const formSubmission = async () => {
+		const values = form.getFieldsValue();
 
+		await emailFormSubmission(values)
+			.then((response: { status: number; message: string }) => {
+				appNotification.success({
+					placement: "top",
+					message: "Email sent successfully",
+					description: response.message,
+				});
+				console.log("Email sent successfully");
+				form.resetFields();
+			})
+			.catch((error: { message: string }) => {
+				appNotification.error({
+					placement: "top",
+					message: "Email sent failed",
+					description: error.message,
+				});
+				console.log("Email sent failed", error);
+			});
+	};
 
-  const calculatePrice = (value: number) => {
-    if (5 <= value && value <= 24) {
-      setPriceValue("$799/Year");
-    } else if (25 <= value && value <= 49) {
-      setPriceValue("$1599/Year");
-    } else if (50 <= value && value <= 99) {
-      setPriceValue("$2599/Year");
-    } else if (100 <= value && value <= 199) {
-      setPriceValue("$3599/Year");
-    } else if (200 <= value && value <= 299) {
-      setPriceValue("$4099/Year");
-    } else if (300 <= value && value <= 399) {
-      setPriceValue("$4899/Year");
-    } else if (400 <= value && value <= 499) {
-      setPriceValue("$5699/Year");
-    } else if (500 <= value && value <= 599) {
-      setPriceValue("$8999/Year");
-    } else if (600 <= value) {
-      setPriceValue("Contact Sales");
-    }
-  };
+	const calculatePrice = (value: number) => {
+		if (5 <= value && value <= 24) {
+			setPriceValue("$799/Year");
+		} else if (25 <= value && value <= 49) {
+			setPriceValue("$1599/Year");
+		} else if (50 <= value && value <= 99) {
+			setPriceValue("$2599/Year");
+		} else if (100 <= value && value <= 199) {
+			setPriceValue("$3599/Year");
+		} else if (200 <= value && value <= 299) {
+			setPriceValue("$4099/Year");
+		} else if (300 <= value && value <= 399) {
+			setPriceValue("$4899/Year");
+		} else if (400 <= value && value <= 499) {
+			setPriceValue("$5699/Year");
+		} else if (500 <= value && value <= 599) {
+			setPriceValue("$8999/Year");
+		} else if (600 <= value) {
+			setPriceValue("Contact Sales");
+		}
+	};
 
-  const marks: SliderSingleProps["marks"] = {
-    5: "5+",
-    25: "25+",
-    50: "50+",
-    100: "100+",
-    200: "200+",
-    300: "300+",
-    400: "400+",
-    500: "500+",
-    600: "600+",
-  };
+	const handleEmployeeNumberChange = (value: number) => {
+		setSelectedValue(value);
+		calculatePrice(value);
+		form.setFieldsValue({
+			numberOfEmployees: `${value}+`,
+		});
+	};
 
-  const radioOptions = [
-    { label: "5+", value: 5 },
-    { label: "25+", value: 25 },
-    { label: "50+", value: 50 },
-    { label: "100+", value: 100 },
-    { label: "200+", value: 200 },
-    { label: "300+", value: 300 },
-    { label: "400+", value: 400 },
-    { label: "500+", value: 500 },
-    { label: "600+", value: 600 },
-  ];
+	useEffect(() => {
+		form.setFieldsValue({
+			numberOfEmployees: selectedValue,
+		});
+	}, [form, selectedValue]);
 
-  const formFields = [
-    {
-      name: "fullName",
-      label: "Full Name",
-      rules: [{ required: true, message: "Please enter your full name" }],
-      prefix: <FaUser />,
-      placeholder: "Full Name *",
-      type: "text",
-    },
-    {
-      name: "companyName",
-      label: "Company Name",
-      rules: [{ required: true, message: "Please enter your company name" }],
-      prefix: <FaBuilding />,
-      placeholder: "Company Name *",
-      type: "text",
-    },
-    {
-      name: "email",
-      label: "Email",
-      rules: [{ required: true, message: "Please enter your email" }],
-      prefix: <FaEnvelope />,
-      placeholder: "Email *",
-      type: "email",
-    },
-    {
-      name: "phone",
-      label: "Phone",
-      rules: [{ required: true, message: "Please enter your phone number" }],
-      prefix: <FaPhoneAlt />,
-      placeholder: "Phone *",
-      type: "tel",
-    },
-  ];
+	return (
+		<div className={styles.pricingFormContainer}>
+			<h2>Pricing Form</h2>
+			<p>Please fill out the form below to get a quote for your company.</p>
+			<p>Required Fields are marked with an asterisk (*)</p>
+			<ConfigProvider
+				theme={{
+					components: {
+						Input: {
+							activeBorderColor: "#0c0050",
+							hoverBorderColor: "#0c0050",
+						},
+						Slider: {
+							trackBg: "#0c0050",
+							trackHoverBg: "#0c0050",
+						},
+						Radio: {
+							buttonSolidCheckedBg: "#0c0050",
+							buttonSolidCheckedHoverBg: "#0c0050",
+						},
+					},
+				}}
+			>
+				<Form
+					form={form}
+					className={styles.pricingForm}
+					name='Pricing Form'
+					initialValues={{ remember: true }}
+					size='large'
+					layout='inline'
+					autoComplete='off'
+					onFinish={formSubmission}
+				>
+					{formFields.map((field) => (
+						<Form.Item
+							key={field.name}
+							name={field.name}
+							className={styles.pricingForm__input}
+							rules={field.rules}
+						>
+							<Input
+								size='large'
+								prefix={field.prefix}
+								placeholder={field.placeholder}
+								type={field.type}
+							/>
+						</Form.Item>
+					))}
+					<Form.Item
+						name='numberOfEmployees'
+						getValueFromEvent={(e) => e.target.value}
+						className={styles.pricingForm__employeeNumber}
+						rules={[
+							{
+								required: true,
+								message: "Please select your number of employees",
+							},
+						]}
+					>
+						<div aria-label='Slider Information'>
+							<p className={styles.pricingForm__employeeNumber__info}>
+								Number of Employees to be Monitored:{" "}
+								<b>{`${selectedValue}+`}</b>
+							</p>
 
-  const handleEmployeeNumberChange = (value: number) => {
-    setSelectedValue(value);
-    calculatePrice(value);
-    form.setFieldsValue({
-      numberOfEmployees: `${value}+`,
-    });
-  };
+							<Slider
+								className={styles.pricingForm__employeeNumber__slider}
+								step={null}
+								marks={marks}
+								min={Number(Object.keys(marks ?? {})[0])}
+								max={Number(Object.keys(marks ?? {}).pop())}
+								value={selectedValue}
+								tooltip={{ open: false }}
+								onChange={(value) => handleEmployeeNumberChange(value)}
+							/>
 
-  useEffect(() => {
-    form.setFieldsValue({
-      numberOfEmployees: selectedValue,
-    });
-  }, [form, selectedValue]);
-
-  return (
-    <div className={styles.pricingFormContainer}>
-      <h2>Pricing Form</h2>
-      <p>Please fill out the form below to get a quote for your company.</p>
-      <p>Required Fields are marked with an asterisk (*)</p>
-      <ConfigProvider
-        theme={{
-          components: {
-            Input: {
-              activeBorderColor: "#0c0050",
-              hoverBorderColor: "#0c0050",
-            },
-            Slider: {
-              trackBg: "#0c0050",
-              trackHoverBg: "#0c0050",
-            },
-            Radio: {
-              buttonSolidCheckedBg: "#0c0050",
-              buttonSolidCheckedHoverBg: "#0c0050",
-            },
-          },
-        }}
-      >
-        <Form
-          form={form}
-          className={styles.pricingForm}
-          name="Pricing Form"
-          initialValues={{ remember: true }}
-          size="large"
-          layout="inline"
-          autoComplete="off"
-          onFinish={formSubmission}
-        >
-          {formFields.map((field) => (
-            <Form.Item
-              key={field.name}
-              name={field.name}
-              className={styles.pricingForm__input}
-              rules={field.rules}
-            >
-              <Input
-                size="large"
-                prefix={field.prefix}
-                placeholder={field.placeholder}
-                type={field.type}
-              />
-            </Form.Item>
-          ))}
-          <Form.Item
-            name="numberOfEmployees"
-            getValueFromEvent={(e) => e.target.value}
-            className={styles.pricingForm__employeeNumber}
-            rules={[
-              {
-                required: true,
-                message: "Please select your number of employees",
-              },
-            ]}
-          >
-            <div aria-label="Slider Information">
-              <p className={styles.pricingForm__employeeNumber__info}>
-                Number of Employees to be Monitored:{" "}
-                <b>{`${selectedValue}+`}</b>
-              </p>
-
-              <Slider
-                className={styles.pricingForm__employeeNumber__slider}
-                step={null}
-                marks={marks}
-                min={Number(Object.keys(marks)[0])}
-                max={Number(Object.keys(marks).pop())}
-                value={selectedValue}
-                tooltip={{ open: false }}
-                onChange={(value) => handleEmployeeNumberChange(value)}
-              />
-
-              <Radio.Group
-                className={styles.pricingForm__employeeNumber__radio}
-                value={selectedValue}
-                options={radioOptions}
-                optionType="button"
-                buttonStyle="solid"
-                onChange={(e) => handleEmployeeNumberChange(e.target.value)}
-              ></Radio.Group>
-              <p className={styles.pricingForm__employeeNumber__cost}>
-                Cost: <b>{priceValue}</b>
-              </p>
-            </div>
-          </Form.Item>
-          <Form.Item className={styles.pricingForm__button}>
-            <Button
-              type="primary"
-              style={{ backgroundColor: "#ce2029" }}
-              htmlType="submit"
-            >
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-        <p>
-          We&apos;ll collect this information subject to our{" "}
-          <a href="/privacy-policy">Privacy Policy</a>.
-        </p>
-      </ConfigProvider>
-    </div>
-  );
+							<Radio.Group
+								className={styles.pricingForm__employeeNumber__radio}
+								value={selectedValue}
+								options={radioOptions}
+								optionType='button'
+								buttonStyle='solid'
+								onChange={(e) => handleEmployeeNumberChange(e.target.value)}
+							></Radio.Group>
+							<p className={styles.pricingForm__employeeNumber__cost}>
+								Cost: <b>{priceValue}</b>
+							</p>
+						</div>
+					</Form.Item>
+					<Form.Item className={styles.pricingForm__button}>
+						<Button
+							type='primary'
+							style={{ backgroundColor: "#ce2029" }}
+							htmlType='submit'
+						>
+							Submit
+						</Button>
+					</Form.Item>
+				</Form>
+				<p>
+					We&apos;ll collect this information subject to our{" "}
+					<a href='/privacy-policy'>Privacy Policy</a>.
+				</p>
+			</ConfigProvider>
+		</div>
+	);
 }
---
+++ libs/utils/conts/pricingFormData.tsx
@@ -0,0 +1,61 @@
+import { SliderSingleProps } from "antd/es/slider";
+import { FaUser, FaBuilding, FaEnvelope, FaPhoneAlt } from "react-icons/fa";
+
+export const formFields = [
+    {
+      name: "fullName",
+      label: "Full Name",
+      rules: [{ required: true, message: "Please enter your full name" }],
+      prefix: <FaUser />,
+      placeholder: "Full Name *",
+      type: "text",
+    },
+    {
+      name: "companyName",
+      label: "Company Name",
+      rules: [{ required: true, message: "Please enter your company name" }],
+      prefix: <FaBuilding />,
+      placeholder: "Company Name *",
+      type: "text",
+    },
+    {
+      name: "email",
+      label: "Email",
+      rules: [{ required: true, message: "Please enter your email" }],
+      prefix: <FaEnvelope />,
+      placeholder: "Email *",
+      type: "email",
+    },
+    {
+      name: "phone",
+      label: "Phone",
+      rules: [{ required: true, message: "Please enter your phone number" }],
+      prefix: <FaPhoneAlt />,
+      placeholder: "Phone *",
+      type: "tel",
+    },
+  ];
+
+  export const marks: SliderSingleProps["marks"] = {
+    5: "5+",
+    25: "25+",
+    50: "50+",
+    100: "100+",
+    200: "200+",
+    300: "300+",
+    400: "400+",
+    500: "500+",
+    600: "600+",
+  };
+
+  export const radioOptions = [
+    { label: "5+", value: 5 },
+    { label: "25+", value: 25 },
+    { label: "50+", value: 50 },
+    { label: "100+", value: 100 },
+    { label: "200+", value: 200 },
+    { label: "300+", value: 300 },
+    { label: "400+", value: 400 },
+    { label: "500+", value: 500 },
+    { label: "600+", value: 600 },
+  ];
