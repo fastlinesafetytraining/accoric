409bbe4873064dbcdb1ad8d893dde43f25dec3ad
Carlos <carlos@flsafety.net>
2025-05-08 15:36:42 -0400

    Enhance mobile navigation bar with new close icon and improved drawer title layout. Update styles for menu items and add hover effects for better user experience.

--- app/Components/UI/NavBar.tsx
+++ app/Components/UI/NavBar.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { Button, Drawer } from "antd";
 import { navMenuItems } from "@/libs/utils/conts/routes";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import styles from "@styles/component.module.scss";
 import PrimaryButton from "./PrimaryButton";
 
@@ -39,19 +39,31 @@ export default function NavBar(): React.ReactElement {
 			{isMobile ?  (
 				// Mobile Navbar
 				<nav className={styles.mobileNavBar}>
-					<Button type='primary' onClick={showMenu}>
+					<Button type='primary' ghost size="large" onClick={showMenu}>
 						<FaBars />
 					</Button>
 					<Drawer
-						title='Welcome to Accoric!'
-						closeIcon={false}
+						title={
+							<div className={styles.mobileNavBar__drawerTitle}>
+								<FaTimes className={styles.mobileNavBar__drawerTitle__closeIcon} onClick={hideMenu} />
+								<h2>Welcome to <span>Accoric</span>!</h2>
+							</div>
+						}
 						onClose={hideMenu}
+						closeIcon={false}
 						open={menuActive}
+						styles={{
+							body: {
+								display: "flex",
+								flexDirection: "column",
+								alignItems: "flex-end",
+							},
+						}}
 					>
 						<ul className={styles.mobileNavBar__menu}>
 							{navMenuItems.map((item) => (
-								<li className={styles.mobileNavBar__menu__Item} key={item.key}>
-									{item.label}
+								<li className={styles.mobileNavBar__menu__item} key={item.key}>
+									<Link className={styles.mobileNavBar__menu__item__link} href={item.href}>{item.label}</Link>
 								</li>
 							))}
 						</ul>
