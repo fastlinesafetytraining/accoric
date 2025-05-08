ebbe71fd0872154c51a97301b099d60b256c1c39
Carlos <carlos@flsafety.net>
2025-05-08 10:55:47 -0400

    Updated faq data for accoric management page

--- app/(pages)/accoric-management/pageData/faq.ts
+++ app/(pages)/accoric-management/pageData/faq.ts
@@ -1,17 +1,24 @@
 const faq = [
   {
     question: "What is Accoric Management?",
-    answer: "Accoric Management is a management system for your business.",
+    answer: "Accoric Management is a management system for your business. It is a platform that allows you to manage your business and its employees by providing certification and training tracking. In order to provided the best work scheduling, we have created a system that allows you to schedule your employees based on their availability and skills.",
   },
   {
-    question: "What is Accoric Management?",
-    answer: "Accoric Management is a management system for your business.",
+    question: "Who is Accoric Management for?",
+    answer: "Accoric Management is for any business that has a large number of employees or a large number of certifications. It is also for for those who would like a better process to productivity and efficiency.",
   },
   {
-    question: "What is Accoric Management?",
-    answer: "Accoric Management is a management system for your business.",
+    question: "What is the cost of Accoric Management?",
+    answer: "Accoric Management is yearly subscription based on the number of employees that you would like to manage.",
   },
-  
+  {
+    question: "How secure is my data with Accoric Management?",
+    answer: "We take data security very seriously. We use the latest encryption methods to ensure that your data is secure. We also have a team of security experts who are constantly monitoring our systems to ensure that your data is safe.",
+  },
+  {
+    question: "How does Accoric Management work?",
+    answer: "Accoric Management is an online web platform that you can access from anywhere. You can use it on your computer, tablet, or phone. You can also use it on your laptop. You can access it from anywhere in the world.",
+  },
 ];
 
 export default faq;
