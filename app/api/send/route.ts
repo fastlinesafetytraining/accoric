eb92fcc5e67224e0fd8df543a46dfdc8c6317a3d
Carlos <carlos@flsafety.net>
2025-05-08 10:54:19 -0400

    Refactor email submission handling by removing the old emailSubmissionApi.tsx file and implementing a new route in send/route.ts. This update enhances the email sending process with improved error handling and integrates the email template directly into the new API route.

---
+++ app/api/send/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+import { EmailHtmlTemplate } from "@components/templates/EmailTemplate";
+import { Resend } from "resend";
+import type { pricingFormInputProps } from "@/types/pricingFormInputProps";
+
+const RESEND_API_KEY = process.env.NEXT_PUBLIC_FORM_RESEND_API_KEY || process.env.RESEND_API_KEY;
+
+if (!RESEND_API_KEY) {
+    throw new Error('Resend API key is not configured');
+}
+
+export async function POST(request: NextRequest) {
+    const resend = new Resend(RESEND_API_KEY);
+    const { fullName, email, companyName, phone, numberOfEmployees } = await request.json() as pricingFormInputProps;
+
+    const emailData = {
+        from: process.env.FROM_EMAIL || "Accoric <webmaster@accoric.com>",
+        to: process.env.TO_EMAIL || "no-reply@accoric.com",
+        subject: `[${fullName}] from ${companyName}, New Email from Accoric Website`,
+        react: EmailHtmlTemplate({ fullName, email, companyName, phone, numberOfEmployees })
+    };
+
+    try {
+        const { data, error } = await resend.emails.send(emailData);
+
+        if (error) {
+            return NextResponse.json({ error: "Email sending failed" }, { status: 500 });
+        }
+
+        return NextResponse.json({ message: "Email sent successfully", data }, { status: 200 });
+
+    } catch (error) {
+        return NextResponse.json({ error: "Email sending failed", details: error }, { status: 500 });
+    }
+}
