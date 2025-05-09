6137b36f563ea92bc73280d497e7c36d092217cc
Carlos <carlos@flsafety.net>
2025-05-09 12:27:08 -0400

    Refactor Features component for improved readability and animation trigger adjustments. Update GSAP scroll trigger settings and clean up code formatting.

--- app/(pages)/accoric-management/pageData/Features.tsx
+++ app/(pages)/accoric-management/pageData/Features.tsx
@@ -7,104 +7,97 @@ import gsap from "gsap";
 import styles from "@styles/accoricManagement.module.scss";
 import { LottieComponent } from "@components/UI";
 import {
-  automationLottie,
-  notificationsLottie,
-  snapshotLottie,
+	automationLottie,
+	notificationsLottie,
+	snapshotLottie,
 } from "@/public/lottie";
 
 export default function Features() {
-  const boxRef = useRef(null);
-  const features = [
-    {
-      Lottieicon: snapshotLottie,
-      title: "Snapshot of your workforce",
-      description:
-        "Easily monitor your emplpoyees' certifications with a user-friendly dashboard.",
-    },
-    {
-      Lottieicon: automationLottie,
-      title: "Automated reminders",
-      description:
-        "Never miss a certification deadline again with our automated reminder system.",
-    },
-    {
-      Lottieicon: notificationsLottie,
-      title: "Customizable notifications",
-      description:
-        "Tailor your notifications to your specific needs and preferences.",
-    },
-  ];
+	const boxRef = useRef(null);
 
-  gsap.registerPlugin(ScrollTrigger);
+	const features = [
+		{
+			Lottieicon: snapshotLottie,
+			title: "Snapshot of your workforce",
+			description:
+				"Easily monitor your emplpoyees' certifications with a user-friendly dashboard.",
+		},
+		{
+			Lottieicon: automationLottie,
+			title: "Automated reminders",
+			description:
+				"Never miss a certification deadline again with our automated reminder system.",
+		},
+		{
+			Lottieicon: notificationsLottie,
+			title: "Customizable notifications",
+			description:
+				"Tailor your notifications to your specific needs and preferences.",
+		},
+	];
+
+	gsap.registerPlugin(ScrollTrigger);
 
   useGSAP(() => {
-    gsap.from(
-      '.managementFeatureItem',
-      {
-        x: '40%',
-        opacity: 0,
-        stagger: 0.3,
-        scrollTrigger: {
-          trigger: boxRef.current,
-          start: "top 20%",
-          end: "10px",
-          scrub: true,
-        },
+    gsap.from(".managementFeatureItem", {
+      x: "40%",
+      opacity: 0,
+      stagger: 0.3,
+      scrollTrigger: {
+        trigger: boxRef.current,
+        start: "top 30%",
+        end: "175px",
+        scrub: true,
       },
-    );
+    });
   });
 
-  return (
-    <section
-      ref={boxRef}
-      id="accoricManagement__features"
-      className={styles.accoricManagement__features}
-    >
-      <div className={styles.accoricManagement__features__sticky}>
-        <h2>Features</h2>
-        <h3>
-          We give you the tools you need to manage your workforce!
-        </h3>
-        <div className={styles.accoricManagement__features__content}>
-          <Image
-            width={645}
-            height={400}
-            className={styles.accoricManagement__features__content__image}
-            src="/Images/accoric-management-tablet-view.webp"
-            alt="A mockup example of accoric management on a tablet device"
-          />
-          <div
-            id="accoricManagement__features__content__list"
-            className={styles.accoricManagement__features__content__list}
-          >
-            {features.map((feature, index) => (
-              <div
-                key={index}
-                className={
-                  `${styles.accoricManagement__features__content__list__item} managementFeatureItem`
-                }
-              >
-                <LottieComponent
-                  animationData={feature.Lottieicon}
-                  className={
-                    styles.accoricManagement__features__content__list__item__icon
-                  }
-                  loop={true}
-                />
-                <div
-                  className={
-                    styles.accoricManagement__features__content__list__item__text
-                  }
-                >
-                  <h3>{feature.title}</h3>
-                  <p>{feature.description}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-
-      </div>
-    </section>
-  );
+	return (
+		<section
+			ref={boxRef}
+			id='accoricManagement__features'
+			className={styles.accoricManagement__features}
+		>
+			<div className={styles.accoricManagement__features__sticky}>
+				<h2>Features</h2>
+				<h3>We give you the tools you need to manage your workforce!</h3>
+				<div className={styles.accoricManagement__features__content}>
+					<Image
+						width={645}
+						height={400}
+						className={styles.accoricManagement__features__content__image}
+						src='/Images/accoric-management-tablet-view.webp'
+						alt='A mockup example of accoric management on a tablet device'
+					/>
+					<div
+						id='accoricManagement__features__content__list'
+						className={styles.accoricManagement__features__content__list}
+					>
+						{features.map((feature, index) => (
+							<div
+								key={index}
+								className={`${styles.accoricManagement__features__content__list__item} managementFeatureItem`}
+							>
+								<LottieComponent
+									animationData={feature.Lottieicon}
+									className={
+										styles.accoricManagement__features__content__list__item__icon
+									}
+									loop={true}
+								/>
+								<div
+									className={
+										styles.accoricManagement__features__content__list__item__text
+									}
+								>
+									<h3>{feature.title}</h3>
+									<p>{feature.description}</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
