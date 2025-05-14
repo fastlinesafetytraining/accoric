57449c87b2a64a41e1845d97cc2400cf184a5a9a
Carlos <carlos@flsafety.net>
2025-05-14 12:12:47 -0400

    Enhance Next.js configuration by adding support for additional page extensions and Sass options. Update Sass version in package files and remove unused variable and mixin imports from multiple SCSS files for cleaner code.

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,27 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 import { redirects } from "@/libs/utils/conts/redirects";
+import path from "path";
 
 /* @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
-  pageExtensions: ["ts", "tsx", "mdx"],
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   /* config options here */
+  sassOptions: {
+    implementation: "sass",
+    additionalData: `
+    @use "./variables.scss" as v;
+    @use "./mixins.scss" as m;
+    `,
+  },
   redirects: async () => [
     ...redirects,
   ],
 };
 
 const withMDX = createMDX({
-  extension: /\.mdx?$/,
+  extension: /\.(md|mdx)$/,
 });
 
 export default withMDX(nextConfig);
