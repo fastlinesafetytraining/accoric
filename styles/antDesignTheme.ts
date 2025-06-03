6b656d64d4c9d2e66f293f925a0936f5a2af054b
fastlinesafetytraining <carlos@flsafety.net>
2025-06-03 16:00:11 -0400

    Made large styling changes to implement dark theme and with the removal of variables.scss the breakpoints have been manually typed in and remove the scss variables to css variables.

---
+++ styles/antDesignTheme.ts
@@ -0,0 +1,67 @@
+import type { ThemeConfig } from "antd";
+import { globalLightGray, globalSecondaryColor, globalPrimaryColor } from "./exportVariables";
+
+const lightTheme: ThemeConfig = {
+    token: {
+        colorPrimary: '#0c0050',
+    },
+    components: {
+        Segmented: {
+            trackBg: globalLightGray,
+        },
+        Form: {
+            labelColor: globalLightGray,
+        },
+        Input: {
+            activeBorderColor: "#0c0050",
+            hoverBorderColor: "#0c0050",
+        },
+        Slider: {
+            trackBg: "#0c0050",
+            trackHoverBg: "#0c0050",
+        },
+        Radio: {
+            buttonSolidCheckedBg: "#0c0050",
+            buttonSolidCheckedHoverBg: "#0c0050",
+        },
+    }
+};
+
+const darkTheme: ThemeConfig = {
+    token: {
+        colorBgElevated: '#1d2044',
+        colorPrimaryHover: globalSecondaryColor,
+    },
+    components: {
+        Segmented: {
+            trackBg: '#222a51',
+            itemColor: '#ffffff',
+            itemHoverColor: '#ffffff',
+            itemSelectedBg: globalSecondaryColor,
+            itemSelectedColor: '#ffffff',
+        },
+        Form: {
+            labelColor: globalLightGray,
+        },
+        Slider: {
+            trackBg: globalPrimaryColor,
+            trackHoverBg: globalPrimaryColor,
+            dotBorderColor: '#ffffff',
+            dotActiveBorderColor: globalPrimaryColor,
+            handleColor: globalPrimaryColor,
+        },
+        Input: {
+            activeBorderColor: globalPrimaryColor,
+            hoverBorderColor: "#0c0050",
+            activeBg: '#3e4162',
+        },
+        Radio: {
+            buttonSolidCheckedBg: globalPrimaryColor,
+            buttonSolidCheckedHoverBg: globalPrimaryColor,
+            buttonBg: 'transparent',
+            colorPrimary: globalPrimaryColor,
+        }
+    }
+};
+
+export { lightTheme, darkTheme };
