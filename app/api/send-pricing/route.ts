1b918c6da283f5b85c2c22e6626488440672456d
Carlos Coreas <ccoreas484@gmail.com>
2025-06-08 13:11:48 -0400

    Renamed file name and Changed email subject and email body title

--- app/api/send-pricing/route.ts
+++ app/api/send-pricing/route.ts
@@ -3,26 +3,27 @@ import type { pricingFormInputProps } from "@/types/pricingFormInputProps";
 const RESEND_API_KEY = process.env.RESEND_API_KEY;
 
 if (!RESEND_API_KEY) {
-	throw new Error("Resend API key is not configured");
+  throw new Error("Resend API key is not configured");
 }
 
 export async function POST(request: NextRequest) {
-	const { fullName, email, companyName, phone, numberOfEmployees } =
-		(await request.json()) as pricingFormInputProps;
+  const { fullName, email, companyName, phone, numberOfEmployees } =
+    (await request.json()) as pricingFormInputProps;
 
-	const res = await fetch("https://api.resend.com/emails", {
-		method: "POST",
-		headers: {
-			Authorization: `Bearer ${RESEND_API_KEY}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			from: `Accoric <${process.env.EMAIL_FROM as string}>`,
-			to: process.env.EMAIL_TO as string,
-			cc: process.env.EMAIL_CC as string,
-			bcc: process.env.EMAIL_BCC as string,
-			subject: `[${fullName}] from ${companyName}, New Email from Accoric Website`,
-			html: `	<body style="width: 100%">
+  const res = await fetch("https://api.resend.com/emails", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${RESEND_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      from: `Accoric <${process.env.EMAIL_FROM as string}>`,
+      to: process.env.EMAIL_TO as string,
+      cc: process.env.EMAIL_CC as string,
+      bcc: process.env.EMAIL_BCC as string,
+      subject: `[${fullName}] from ${companyName}, New Management Pricing Request`,
+      html: `
+	  <body style="width: 100%">
 		<table border="0" max-width="600px" margin="0 auto" bgcolor="#ffffff" cellpadding="0" cellspacing="0">
 			<tr>
 				<td padding="20px" border="0">
@@ -41,7 +42,7 @@ export async function POST(request: NextRequest) {
 					<tr>
 						<td padding="20px 0">
 							<h1 color="#333333" align="center" line-height="1.5" margin="0">
-								New Email from Accoric Website.
+								New Accoric Management Pricing Request.
 							</h1>
 						</td>
 					</tr>
@@ -83,23 +84,23 @@ export async function POST(request: NextRequest) {
 			</tr>
 		</table>
 	</body>`,
-		}),
-	});
+    }),
+  });
 
-	if (!res.ok) {
-		return NextResponse.json(
-			{ error: "Email sending failed" },
-			{ status: 500 }
-		);
-	} else if (res.ok) {
-		return NextResponse.json(
-			{ message: "Email sent successfully" },
-			{ status: 200 }
-		);
-	} else {
-		return NextResponse.json(
-			{ error: "Email sending failed, please try again later" },
-			{ status: 500 }
-		);
-	}
+  if (!res.ok) {
+    return NextResponse.json(
+      { error: "Email sending failed" },
+      { status: 500 }
+    );
+  } else if (res.ok) {
+    return NextResponse.json(
+      { message: "Email sent successfully" },
+      { status: 200 }
+    );
+  } else {
+    return NextResponse.json(
+      { error: "Email sending failed, please try again later" },
+      { status: 500 }
+    );
+  }
 }
