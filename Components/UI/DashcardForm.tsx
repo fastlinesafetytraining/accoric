217340c89de8ce830d3fa9ab3204675c5c0956e6
fastlinesafetytraining <carlos@flsafety.net>
2025-06-09 10:39:06 -0400

    Implement email sending functionality in DashcardForm component; add success and error notifications using react-hot-toast, and reset form fields upon submission.

--- Components/UI/DashcardForm.tsx
+++ Components/UI/DashcardForm.tsx
@@ -1,14 +1,24 @@
 "use client";
 import React, { useEffect } from "react";
 import { Flex, Form, Input, Button } from "antd";
+import toast from "react-hot-toast";
 import styles from "@styles/component.module.scss";
 
 export default function DashcardForm() {
   const [form] = Form.useForm();
 
   const handleSubmit = async () => {
-    const values = await form.validateFields();
-    console.log(values);
+    const response = await fetch("/api/send-dashcard-email", {
+      method: "POST",
+      body: JSON.stringify(form.getFieldsValue()),
+    });
+    if (!response.ok) {
+      toast.error("Email sent failed");
+      form.resetFields();
+    } else {
+      toast.success("Email sent successfully");
+      form.resetFields();
+    }
   };
 
   useEffect(() => {
