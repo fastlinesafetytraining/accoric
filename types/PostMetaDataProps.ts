57c7b0e02cd682d61c115d8bcf847336d4511c5c
fastlinesafetytraining <carlos@flsafety.net>
2025-07-01 15:59:56 -0400

    Add getPostMetaData utility to convert makrdown files to dynamic pages and PostMetaDataProps type for handling blog post metadata

---
+++ types/PostMetaDataProps.ts
@@ -0,0 +1,10 @@
+export interface PostMetaDataProps {
+    title: string;
+    date: string;
+    excerpt: string;
+    slug: string;
+    author: string;
+    content: string;
+    image: string;
+    imageAlt: string;
+}
