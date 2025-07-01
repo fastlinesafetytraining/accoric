5045d8a0488d6b8204f84535618ff0343260bfc8
fastlinesafetytraining <carlos@flsafety.net>
2025-07-01 15:59:56 -0400

    Add getPostMetaData utility to convert makrdown files to dynamic pages and PostMetaDataProps type for handling blog post metadata

---
+++ libs/utils/getPostMetaData.ts
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+import { PostMetaDataProps } from '@/types/PostMetaDataProps'
+
+export default function getPostMetaData(): PostMetaDataProps[] {
+    const folder = 'posts/'
+    const files = fs.readdirSync(folder)
+    const onlyMarkdownPosts = files.filter((file) => file.endsWith('.md') || file.endsWith('.mdx'))
+
+    // Get Gray matter data from each file
+    const posts = onlyMarkdownPosts.map((fileName) => {
+        const fileContents = fs.readFileSync(`posts/${fileName}`, 'utf8')
+        const matterResult = matter(fileContents)
+        return {
+            title: matterResult.data.title,
+            date: matterResult.data.date,
+            excerpt: matterResult.data.excerpt,
+            slug: fileName.replace('.mdx', '').replace('.md', ''),
+            author: matterResult.data.author,
+            content: matterResult.content,
+            image: matterResult.data.image,
+            imageAlt: matterResult.data.imageAlt,
+        }
+    })
+
+    return posts.sort((a, b) => {
+        return new Date(b.date).getTime() - new Date(a.date).getTime()
+    })
+}
