2d4520f2232ea5363408bbcda390c50fc7367faa
fastlinesafetytraining <carlos@flsafety.net>
2025-07-14 12:07:56 -0400

    Implement dynamic header background color change on scroll and adjust main padding in global styles

--- Components/UI/Header.tsx
+++ Components/UI/Header.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { SiteButton, NavBar, ThemeToggle } from ".";
@@ -6,8 +7,29 @@ import { LuUser } from "react-icons/lu";
 import "@styles/globals.css";
 
 export default function Header() {
+	const [bgTransparent, setBgTransparent] = useState(true);
+
+	// ** Header Background Color Change on Scroll **
+	useEffect(() => {
+		const handleScroll = () => {
+			setBgTransparent(window.scrollY > 100);
+			console.log(window.scrollY)
+		};
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
 	return (
-		<header className='flex justify-between items-center'>
+		<header
+			style={{
+				backgroundColor: bgTransparent
+					? "var(--header-footer-background-color)"
+					: "transparent",
+			}}
+			className='flex justify-between items-center sticky top-0 z-50'
+		>
 			<Link href='/'>
 				<Image
 					unoptimized
