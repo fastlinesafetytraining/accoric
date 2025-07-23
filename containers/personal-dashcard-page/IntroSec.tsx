c1912d51c77a1b7e5c919a369f65db7f530c944c
fastlinesafetytraining <carlos@flsafety.net>
2025-07-23 16:36:56 -0400

    Refactor IntroSec component to enhance scroll-based animations, update layout structure, and improve image quality for better visual engagement.

--- containers/personal-dashcard-page/IntroSec.tsx
+++ containers/personal-dashcard-page/IntroSec.tsx
@@ -1,53 +1,66 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
-import { motion, useScroll } from "motion/react";
-import useParallax from "@/Components/hook/useParallax";
+import { motion, useScroll, useTransform } from "motion/react";
 import styles from "@styles/pages/personalDashcard.module.scss";
 import SiteButton from "@/Components/UI/SiteButton";
 import WelderSparks from "@/Components/UI/WelderSparks";
 
 export default function IntroSec() {
-	const { scrollYProgress } = useScroll();
-	const y = useParallax(scrollYProgress, 50);
+	const ref = useRef<HTMLDivElement>(null);
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["start 0%", "end 80%"],
+	});
 
 	return (
-		<section id='introduction' className={styles.introSec}>
-			<div className={styles.introSec__hero}>
+		<section ref={ref} id='introduction' className={styles.introSec}>
+			<div className={styles.introSec__content}>
 				<motion.div
 					initial={{ opacity: 0, x: -100 }}
 					animate={{ opacity: 1, x: 0 }}
 					transition={{ duration: 0.5 }}
-					className={styles.introSec__hero_text}
+					className={styles.introSec__content__text}
 				>
-					<h1>
-						You <span>Provide.</span>
-						<br /> We <span>Protect.</span>
-					</h1>
+					<motion.h1
+						initial={{ opacity: 0, x: -100 }}
+						animate={{ opacity: 1, x: 0 }}
+						transition={{ duration: 0.5, staggerChildren: 0.5 }}
+						exit={{ opacity: 0, y: 100 }}
+					>
+						You
+						<br /> <span>Provide.</span>
+						<br /> We
+						<br /> <span>Protect.</span>
+					</motion.h1>
 					<p>
-						With NYC&apos;s first personal certification tracker,<br /> we&apos;re protecting-what lets you work for.
+						With NYC&apos;s first personal certification tracker,
+						<br /> we&apos;re protecting what lets you work — and what you work
+						for.
 					</p>
-					<SiteButton type='secondary' href='/signup' title='Sign Up'>
-						Sign Up
-					</SiteButton>
+					<div className={styles.introSec__content__buttonContainer}>
+						<SiteButton type='secondary' href='/signup' title='Sign Up'>
+							Sign Up
+						</SiteButton>
+					</div>
 				</motion.div>
-			</div>
-			<div className={styles.introSec__parallax}>
-				<motion.div
-					initial={{ opacity: 0, y: 100 }}
-					animate={{ opacity: 1, y: 0 }}
-					transition={{ duration: 1.5 }}
-					style={{ y }}
-					className={styles.introSec__parallax__item_2}
-				>
+				<div className={styles.introSec__content__assetContainer}>
 					<Image
-						src='/Images/welder-image.webp'
+						className={styles.introSec__content__assetContainer__img}
+						src='/Images/personal-dashcard/welder-image.webp'
 						alt='welder-image'
-						width={1200}
-						height={500}
+						width={700}
+						height={700}
 					/>
 					<WelderSparks />
-				</motion.div>
+					<Image
+						className={styles.introSec__content__assetContainer__imgBackground}
+						src='/Images/personal-dashcard/welding-background.webp'
+						alt='welder-image'
+						width={700}
+						height={700}
+					/>
+				</div>
 			</div>
 		</section>
 	);
