8f58006963b8c738115295a3388330235a0dc25f
fastlinesafetytraining <carlos@flsafety.net>
2025-07-29 13:49:09 -0400

    Update CallToActionSec component by changing text and images

--- containers/personal-dashcard-page/CallToActionSec.tsx
+++ containers/personal-dashcard-page/CallToActionSec.tsx
@@ -1,19 +1,31 @@
-"use client";
-import { SiteButton } from "@/Components/UI";
 import React from "react";
+import Image from "next/image";
 import styles from "@styles/pages/personalDashcard.module.scss";
+import { Divider } from "antd";
 
 export default function CallToActionSec() {
   return (
     <section id="register" className={styles.callToActionSec}>
       <div className={styles.callToActionSec__content}>
         <h2 className={styles.callToActionSec__content__heading}>
-          Ready to get your Personal DASHcard™?
+          Ready to get started with your DASHcard?
         </h2>
-        <p className={styles.callToActionSec__content__subHeading}>
-          Combine all your certifications into one smart card and never miss an expiration again.
+        <Divider size="small" />
+        <p className={styles.callToActionSec__content__paragraph}>
+          Download the Accoric DASHcard app now to get started. Sign in with your Accoric account and start using your DASHcard.
+          Available on the App Store and Google Play.<br/>
         </p>
-        <SiteButton type="primary" href="/signup" title="Get Started">Get Started</SiteButton>
+        <span className={styles.callToActionSec__content__paragraph__note}>
+          (Click the image below to be redirected to the App Store or Google Play)
+        </span>
+        <div className={styles.callToActionSec__content__buttonContainer}>
+          <a href="/" target="_blank" rel="noopener noreferrer">
+            <Image src="/svg/apple-app-store-download.svg" alt="Download Accoric App on the Apple App Store" width={200} height={200} />
+          </a>
+          <a href="/" target="_blank" rel="noopener noreferrer">
+            <Image src="/svg/android-play-store-download.svg" alt="Get the Accoric App on Google Play" width={200} height={200} />
+          </a>
+        </div>
       </div>
     </section>
   );
