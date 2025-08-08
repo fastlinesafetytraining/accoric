74ac29596a8038bab231356ab243afb3b619f218
fastlinesafetytraining <carlos@flsafety.net>
2025-08-08 13:03:24 -0400

    Enhance IntroSec component to support WebGL detection, conditionally render Spline or fallback image based on support. Update sign-up link for better navigation.

--- containers/personal-dashcard-page/IntroSec.tsx
+++ containers/personal-dashcard-page/IntroSec.tsx
@@ -1,12 +1,27 @@
 "use client";
-import React from "react";
-import { SplineScene } from "@/Components/UI";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import hasWebGlSupport from "@/libs/utils/checkWebGl";
+import Spline from "@splinetool/react-spline";
 import styles from "@styles/pages/personalDashcard.module.scss";
 import SiteButton from "@/Components/UI/SiteButton";
 
 export default function IntroSec() {
+	const [isWebGLSupported, setIsWebGLSupported] = useState(false);
+
+	useEffect(() => {
+		setIsWebGLSupported(hasWebGlSupport());
+	}, []);
+
 	return (
 		<section id='introduction' className={styles.introSec}>
+			<div className={styles.introSec__background}>
+				{isWebGLSupported === true ? (
+					<Spline
+						scene={process.env.NEXT_PUBLIC_DASHCARD_SCENE_HERO as string}
+					/>
+				) : null}
+			</div>
 			<div className={styles.introSec__content}>
 				<div className={styles.introSec__content__text}>
 					<h1>
@@ -21,21 +36,27 @@ export default function IntroSec() {
 						for.
 					</p>
 					<div className={styles.introSec__content__buttonContainer}>
-						<SiteButton type='secondary' href='/signup' title='Sign Up'>
+						<SiteButton
+							type='secondary'
+							href='/sign-up/personal-dashcard'
+							title='Sign Up'
+						>
 							Sign Up
 						</SiteButton>
 					</div>
 				</div>
-				<div className={styles.introSec__content__assetContainer}>
-					<SplineScene
-						className={styles.introSec__content__assetContainer__spline}
-						sceneId={process.env.NEXT_PUBLIC_DASHCARD_SCENE_HERO as string}
-						alt='Construction worker holding a child with arms looking at the construction site background'
-						fallbackImage='/Images/personal-dashcard/hero-image.png'
-						width={600}
-						height={600}
-					/>
-				</div>
+
+				{isWebGLSupported === false ? (
+					<div className={styles.introSec__content__assetContainer}>
+						<Image
+							src='/Images/personal-dashcard/hero-image.png'
+							alt='Construction worker holding a child with arms looking at the construction site background'
+							width={600}
+							height={600}
+							sizes='(max-width: 768px) 400px'
+						/>
+					</div>
+				) : null}
 			</div>
 		</section>
 	);
