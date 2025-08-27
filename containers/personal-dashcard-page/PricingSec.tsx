6349755f288de3c92fb9d400b5b587d3fbfcbf2e
fastlinesafetytraining <carlos@flsafety.net>
2025-08-27 12:29:49 -0400

    removed physical card fee as per management

--- containers/personal-dashcard-page/PricingSec.tsx
+++ containers/personal-dashcard-page/PricingSec.tsx
@@ -3,45 +3,47 @@ import styles from "@/styles/pages/personalDashcard.module.scss";
 import { SiteButton } from "@/Components/UI";
 
 export default function PricingSec() {
-
 	return (
-		<section id='pricing' className={styles.pricingSec}>
+		<section id='pricing' aria-label='Pricing' className={styles.pricingSec}>
 			<div className={styles.pricingSec__content}>
 				<h2>Pricing</h2>
 				<div className={styles.pricingSec__content__cardContainer}>
 					<div
-						className={`${styles.pricingSec__content__cardContainer__card} ${styles.pricingSec__content__cardContainer__card__monthly}`}
+						className={`${styles.pricingSec__content__cardContainer__card} ${styles.pricingSec__content__cardContainer__card}`}
 					>
 						<div
-
+							className={
+								styles.pricingSec__content__cardContainer__card__header
+							}
 						>
-							<h3>Monthly Subscription</h3>
-							<h4
-							>
-								$10.99/month<br />
-								<span>
-									+ $25.00 One Time Card Issuance Fee
-								</span>
+							<h3>Personal Dashcard <br/> Standard Subscription</h3>
+							<h4>
+								10<span>99 /Month</span>
+								<br />
 							</h4>
 						</div>
-
-					<ul>
-							<li>Access your certifications anytime, anywhere</li>
-							<li>Text and email notifications</li>
-							<li>Automatic expiration reminders</li>
-							<li>Secure cloud storage for your credentials</li>
-							<li>Easy sharing with employers and clients</li>
-							<li>Simple, user-friendly dashboard</li>
-							<li>Priority customer support</li>
-							<li>Cancel Subscription Anytime</li>
-						</ul>
-						<SiteButton
-							href='/sign-up/personal-dashcard'
-							title='Sign Up'
-							type='primary'
+						<div
+							className={styles.pricingSec__content__cardContainer__card__body}
 						>
-							Sign Up
-						</SiteButton>
+							<ul>
+								<li>Physical Card included in your subscription</li>
+								<li>Access your certifications anytime, anywhere</li>
+								<li>Text and email notifications</li>
+								<li>Automatic expiration reminders</li>
+								<li>Secure cloud storage for your credentials</li>
+								<li>Easy sharing with employers and clients</li>
+								<li>Simple, user-friendly dashboard</li>
+								<li>Priority customer support</li>
+								<li>Cancel Subscription Anytime</li>
+							</ul>
+							<SiteButton
+								href='/sign-up/personal-dashcard'
+								title='Sign Up'
+								type='primary'
+							>
+								Sign Up
+							</SiteButton>
+						</div>
 					</div>
 				</div>
 			</div>
